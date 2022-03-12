70c85093c42805319ad667fed9144c473bc6defa
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-12 17:36:17 -0500

    Setup outline for GraphQL queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+const { AuthenticationError } = require('apollo-server-express');
+
+
+const resolvers = {
+
+}
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gqp`
+
+`;
+
+module.exports = typeDefs;
