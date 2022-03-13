c8328da2c3e072640121253e75678b95ed5c3069
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-13 11:45:34 -0400

    Setup scale-able game stat tracking

---
+++ server/config/gameModels.js
@@ -0,0 +1,33 @@
+// Default stats to track for all games
+const defaultStats = {
+	// Best score for that game
+	bestScore: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	// Number of times that game is played
+	playedGames: {
+		type: Number,
+		required: true,
+		default: 0
+	}
+}
+
+// Stores each game's stat trackers
+const gameDictionary = {
+	tetris: {
+		...defaultStats,
+		// Store number of rows cleared in tetris
+		rowsCleared: {
+			type: Number,
+			required: true,
+			default: 0
+		}
+	},
+	yahtzee: {
+		...defaultStats,
+	}
+};
+
+module.exports = gameDictionary;
