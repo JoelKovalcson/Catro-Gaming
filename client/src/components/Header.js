3426bef0c95fb19c409ea355e1fc4fb8ea2077ef
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-15 21:15:14 -0400

    Created navigation component skeleton

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 const Header = () => {
 	return (
 		<>
 			
+			<Navigation/>
 		</>
 	)
 }
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<>
+			
+		</>
+	)
+}
+
+export default Navigation;
