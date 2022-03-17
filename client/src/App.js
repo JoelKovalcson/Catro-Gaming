aa714e4facd31bd6dccf3fb8c0c652a6046f0fb9
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-17 16:29:02 -0400

    Fixed stupid error, apollo/client could really use some work

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import SignUp from './pages/SignUp';
 import Tetris from './pages/Tetris';
 
 const httpLink = createHttpLink({
-	uri: '/graphql',
+	uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
