9fe944b9a2e5e189a094a4b82f00e9bc0e0518ac
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-03-18 09:27:57 -0500

    homepage added

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
 	return (
 		<>
-			<Header/>
+			<div className='flex justify-center my-6'>
+				<div className='flex flex-col w-1/4 rounded border border-8 border-double border-pastel-purple bg-light-background'>
+					<h3 className='self-center text-xl'>Pick your category:</h3>
+					<button className='self-center w-3/4 h-12 m-4 rounded bg-pastel-purple'>
+						<Link to="/singleplayer" className='text-black'>Single Player</Link>
+					</button>
+					<button className='self-center w-3/4 h-12 m-4 rounded bg-pastel-purple'>
+						<Link to="/multiplayer" className='text-black'>Mulitplayer</Link>	
+					</button>		
+				</div>
+			</div>
 		</>
 	)
 }
