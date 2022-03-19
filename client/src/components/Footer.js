4b3e0ac2002cdb395d18050772985804e7f91877
Woogster <john.m.wooley@gmail.com>
2022-03-19 03:04:00 -0500

    Added little footer, started dropdown menu...bedtime

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<>
-			<h3> Temporary footer area. LOL </h3>
+			<div className='absolute inset-x-0 bottom-0 h-16 flex flex-col sm:flex-row justify-center border-t-2 '>
+				<h3 className='flex justify-center sm:m-auto'>Brought to you by Team 1</h3>
+			</div>
 			
 		</>
 	)
