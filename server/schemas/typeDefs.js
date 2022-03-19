a8e1c2ec405248e66006094f3b9eb033a32ca12c
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-19 17:23:13 -0400

    Formatting changes and removed a console log

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -67,7 +67,7 @@ const typeDefs = gql`
 		users: [User]
 		getGame(gameId: ID!): ActiveGame
 		getProfile(userId: ID!): User
-		getJoinableGames: [ActiveGame] 
+		getJoinableGames: [ActiveGame]
 	}
 
 	type Mutation {
