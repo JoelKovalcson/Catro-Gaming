60abe7197bbe56748acdb552bd347ec998ddc0c5
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2022-03-19 12:41:16 -0500

    dice roll random

--- client/src/pages/Yahtzee.js
+++ client/src/pages/Yahtzee.js
@@ -1,31 +1,21 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Lowerscore from '../components/game/yahtzee/Lowerscore';
+import Rolldice from '../components/game/yahtzee/Rolldice';
 import Upperscore from '../components/game/yahtzee/Upperscore';
-import Dice from '../components/game/yahtzee/Dice';
-import { useState } from 'react';
 
-const Yahtzee = (props) => {
-	const [roll, setRoll] = useState(1);
-	let counter = 0;
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			counter += 1;
-			if(counter >= 7){
-				clearInterval(interval);
-			}
-			setRoll(Math.floor(Math.random()*5)+1);
-		}, 100);
-	}, [props.isIsolated]);
-
+const Yahtzee = () => {
 	return (
 		<>
 			<div className='flex flex-wrap justify-evenly'>
 				<Upperscore/>
 				<Lowerscore/>
 				<div className='flex flex-row sm:flex-row md:flex-col lg:flex-col justify-center m-4'>
-					<Dice roll={roll}/>
-					<Dice roll={roll}/>
+					<Rolldice/>
+					<Rolldice/>
+					<Rolldice/>
+					<Rolldice/>
+					<Rolldice/>
+					<Rolldice/>
 				</div>	
 			</div>
 			<div className='flex justify-center'>
