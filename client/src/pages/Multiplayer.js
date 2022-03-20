f2f4a107cfb06c3eeb2cdb7c1c34775ce2d2b0db
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-19 23:37:44 -0400

    Setup dynamic card loading of each multiplayer game

--- client/src/pages/Multiplayer.js
+++ client/src/pages/Multiplayer.js
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {MULTIPLAYER_GAMES} from '../utils/homepage/games';
 
 const Multiplayer = () => {
 
-	const [gameSelection, setGameSelection] = useState({game: 'yahtzee', maxPlayers: 2, showModal: true, gameId: '', MIN: 1, MAX: 5});
+	const [gameSelection, setGameSelection] = useState({game: 'yahtzee', maxPlayers: 2, showModal: false, gameId: '', MIN: 1, MAX: 5});
 	const [joinGameArr, setJoinGameArr] = useState([]);
-
+	
 	const handleFormSubmit = (event) => {
 		event.preventDefault();
 	}
 
 	const handleChange = (event) => {
+		event.preventDefault();
+		if(event.target.name === 'background') {
+			setGameSelection({
+				...gameSelection,
+				showModal: false
+			})
+			return;
+		}
+		else {
+			console.log(event.target)
+		}
 		setGameSelection({
 			...gameSelection,
 			[event.target.name]: event.target.value
 		});
 	}
 
+	const createGame = (event) => {
+		event.preventDefault();
+		console.log(MULTIPLAYER_GAMES);
+	}
+
 	return (
 		<>
 			{/* Modal */}
-			<div className={`${(gameSelection.showModal) ? "visible" : "invisible"} grid z-40 bg-background/50 fixed top-0 bottom-0 right-0 left-0 justify-center items-center overflow-y-auto overflow-x-hidden h-modal md:h-full md:inset-0`}>
+			<a href="#0" onClick={handleChange} name="background" className={`${(gameSelection.showModal) ? "visible" : "invisible"} grid z-40 bg-background/[.85] fixed top-0 bottom-0 right-0 left-0 justify-center items-center overflow-y-auto overflow-x-hidden h-modal md:h-full md:inset-0`}>
 				<form onSubmit={handleFormSubmit} className="p-5 bg-light-background rounded-lg max-w-sm border-4 border-double border-pastel-purple">
-					<div className="mb-6 grid">
+					<div className="grid">
 						<div className="text-center inline-block mb-2 text-light-blue border-b border-pastel-purple">
 							Game Selected
 						</div>
@@ -60,76 +77,69 @@ const Multiplayer = () => {
 							transition
 							ease-in-out
 							m-0
-							focus:outline-none" 
+							focus:outline-none
+							mb-6" 
 							id="maxPlayers"
 							placeholder="1"/>
+						<button type="submit"  className="
+							w-full
+							px-6
+							py-2.5
+							bg-light-background
+							border-4
+							border-double
+							border-light-blue
+							text-light-blue
+							font-medium
+							text-sm
+							font-bold
+							leading-tight
+							uppercase
+							rounded
+							shadow-md
+							hover:bg-dark-blue hover:shadow-lg
+							focus:bg-dark-blue focus:shadow-lg focus:outline-none focus:ring-0
+							active:bg-blue-800 active:shadow-lg
+							transition
+							duration-150
+							ease-in-out"
+						>
+							Create Game
+						</button>
 					</div>
 					
-					<button type="submit"  className="
-						w-full
-						px-6
-						py-2.5
-						bg-light-background
-						border-4
-						border-double
-						border-light-blue
-						text-light-blue
-						font-medium
-						text-sm
-						font-bold
-						leading-tight
-						uppercase
-						rounded
-						shadow-md
-						hover:bg-dark-blue hover:shadow-lg
-						focus:bg-dark-blue focus:shadow-lg focus:outline-none focus:ring-0
-						active:bg-blue-800 active:shadow-lg
-						transition
-						duration-150
-						ease-in-out"
-					>
-						Create Game
-					</button>
+					
 				</form>
-			</div>
+			</a>
 			
 			{/* Game Selection */}
-			<div>
-
-			</div>
+			<section>
+				<div className="text-center text-3xl mb-10">
+					CREATE GAME
+				</div>
+				<div className="flex flex-wrap justify-center w-screen">
+					{
+						MULTIPLAYER_GAMES.map((game) => {
+							return (
+								<a key={game.name} name={game.name} data-min-players={game.minPlayers} data-max-players={game.maxPlayers} data-route={game.route} onClick={createGame} href="#0" className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
+									<div className="card-text self-center text-bold text-xl">{game.name}</div>
+								</a>
+							)
+						})
+					}
+				</div>
+			</section>
 			{/* Join Game Listings */}
-			<div>
+			<div className="border-t-4 my-10"/>
+			<section className="">
+				<div>
 
-			</div>
-			<div className="flex flex-wrap justify-center w-screen">
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-
-				<Link to='/yahtzee' className="card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-chuck-norris">    
-					<div className="card-text self-center text-bold text-xl">Yahtzee</div>
-				</Link>
-			</div>
+				</div>
+				<div className="flex flex-wrap justify-center w-screen">
+					Content
+				</div>
+			</section>
+			
 		</>
 	)
 }
