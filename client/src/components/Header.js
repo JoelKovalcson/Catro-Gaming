35fc55d9581858ce21fade4d59694504835c0030
Woogster <john.m.wooley@gmail.com>
2022-03-20 03:12:09 -0500

    Added gaming for lg screens.. will ahve logo later

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -42,10 +42,13 @@ const Header = () => {
 	// else {
 		return (
 			<header className='flex flex-col md:border-b-4 md:border-double md:border-pastel-purple md:flex-row md:justify-between h-96 md:h-48'>
-				<Link to='/' className='sm:w-full border-b-4 border-double border-pastel-purple md:border-none h-96 sm:h-48 bg-logo bg-center bg-contain bg-no-repeat md:bg-right lg:bg-left lg:w-1/2 text-center p-4 '>
+				<Link to='/' className='sm:w-full border-b-4 border-double md:w-3/5 border-pastel-purple md:border-none h-96 md:h-48 bg-logo bg-center bg-contain bg-no-repeat md:bg-right lg:bg-left lg:w-1/2 text-center p-4 '>
 					{/* <Dropdown/> */}
 				</Link>
-				<div className='flex justify-center md:justify-end lg:hidden w-full md:w-1/5 md:ml-24 border-b-4 md:border-none border-double border-pastel-purple p-4'>
+				<div className='m-auto'>
+					<h1>Gaming</h1>
+				</div>
+				<div className='flex justify-center md:justify-end lg:hidden w-full sm:mt-0 md:w-1/5 md:ml-24 border-b-4 md:border-none border-double border-pastel-purple p-4'>
 					<button id='dropdownbutton' data-dropdown-toggle='dropdown' className='text-4xl m-auto lg:invisible hover:text-dark-blue hover:shadow-lg
 						focus:text-light-blue focus:shadow-lg focus:outline-none focus:ring-0
 						active:text-blue-800 active:shadow-lg
