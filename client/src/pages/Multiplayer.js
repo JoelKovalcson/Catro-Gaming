bb75d9269ef3b7348b71da0303591d9fec01be58
Joel Kovalcson <joelkovalcson@gmail.com>
2022-03-24 06:48:17 -0400

    Testing if this fixes error

--- client/src/pages/Multiplayer.js
+++ client/src/pages/Multiplayer.js
@@ -237,17 +237,22 @@ const Multiplayer = () => {
 						</div>
 						// Else they are loaded
 						:
-						joinableGamesQuery.getJoinableGames.map((game) => {
-							return (
-								<a href="#0" onClick={joinGame} key={game._id} data-game-id={game._id} data-game-name={game.gameName} data-max-players={game.maxPlayers} data-cur-players={game.participants.length}
-									className={`card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-${game.gameName}`}>    
-									<div className="card-text self-center text-bold text-xl">{game.gameName.charAt(0).toUpperCase() + game.gameName.slice(1)}</div>
-									<div className="text-light-blue card-text self-center text-bold text-l">Host: {game.participants[0].username}</div>
-									<div className="text-light-blue card-text self-center text-bold text-l">{game.participants.length}/{game.maxPlayers} Players</div>
-								</a>
-							)
-						})
-						
+							((joinableGamesQuery.getJoinableGames)
+							?
+								joinableGamesQuery.getJoinableGames.map((game) => {
+									return (
+										<a href="#0" onClick={joinGame} key={game._id} data-game-id={game._id} data-game-name={game.gameName} data-max-players={game.maxPlayers} data-cur-players={game.participants.length}
+											className={`card-image flex flex-col justify-center mx-4 mt-4 h-48 w-48 rounded bg-cover bg-center bg-${game.gameName}`}>    
+											<div className="card-text self-center text-bold text-xl">{game.gameName.charAt(0).toUpperCase() + game.gameName.slice(1)}</div>
+											<div className="text-light-blue card-text self-center text-bold text-l">Host: {game.participants[0].username}</div>
+											<div className="text-light-blue card-text self-center text-bold text-l">{game.participants.length}/{game.maxPlayers} Players</div>
+										</a>
+									)
+								})
+							: 
+								<div className="text-blue-500 h-48 mx-4 mt-4 text-xl">
+									You have no games!
+								</div>)
 					}
 				</div>
 			</section>
